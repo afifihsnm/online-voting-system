e64075ef5f8931210a9bcf459649b3e4fdd03a8a
afifihsnm <afifihsanke2@gmail.com>
2025-07-19 17:12:22 +0700

    fix: fix styling

--- resources/js/pages/events/detail.tsx
+++ resources/js/pages/events/detail.tsx
@@ -57,8 +57,7 @@ export default function DetailEvent({ event }: Props) {
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <Card>
                     <CardHeader>
-                        <CardTitle>Bar Chart - Custom Label</CardTitle>
-                        <CardDescription>January - June 2024</CardDescription>
+                        <CardTitle>Bar Chart Vote Result</CardTitle>
                     </CardHeader>
                     <CardContent>
                         <ChartContainer config={chartConfig}>
@@ -88,12 +87,6 @@ export default function DetailEvent({ event }: Props) {
                             </BarChart>
                         </ChartContainer>
                     </CardContent>
-                    <CardFooter className="flex-col items-start gap-2 text-sm">
-                        <div className="flex gap-2 leading-none font-medium">
-                            Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                        </div>
-                        <div className="leading-none text-muted-foreground">Showing total visitors for the last 6 months</div>
-                    </CardFooter>
                 </Card>
             </div>
         </AppLayout>
